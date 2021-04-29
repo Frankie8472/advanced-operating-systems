d542d6bf0602e06d0b2a676a6edf2653c489dbb1
Nicolas Winkler <nicolas.winkler@gmx.ch>
2021-04-29 21:18:38 +0200

    add include guards to test.h

--- usr/init/test.h
+++ usr/init/test.h
@@ -1,6 +1,13 @@
+#ifndef INIT_TEST_H_
+#define INIT_TEST_H_
+
+
 // macro to put at start of each test-function:
 #define TEST_START debug_printf("======================\n");\
     debug_printf("RUNNING TEST: %s\n", __func__);\
     debug_printf("======================\n");
 
 int run_init_tests(int core_id);
+
+
+#endif // INIT_TEST_H_
