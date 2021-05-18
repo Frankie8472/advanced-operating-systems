a8be1ef0c4e677dd31f3d57f960fe3cbf390bb43
nigglir <nigglir@student.ethz.ch>
2021-05-18 18:19:43 +0200

    updated function signatures, maybe it will soon actually do something

--- usr/drivers/enet/enet_handler.c
+++ usr/drivers/enet/enet_handler.c
@@ -38,18 +38,42 @@ static void print_arp_packet(struct arp_hdr* h) {
     ENET_DEBUG("hwlen - %d\n", h->hwlen);
     ENET_DEBUG("protolen - %d\n", h->protolen);
     ENET_DEBUG("opcode - %d\n", ntohs(h->opcode));
-    ENET_DEBUG("ip_src - %d\n", ntohs(h->ip_src));
-    ENET_DEBUG("ip_dst - %d\n", ntohs(h->ip_dst));
+    ENET_DEBUG("ip_src - %d\n", ntohl(h->ip_src));
+    ENET_DEBUG("ip_dst - %d\n", ntohl(h->ip_dst));
+    ENET_DEBUG("eth_src:\n");
+    for (int i = 0; i < 6; i++)
+        ENET_DEBUG("%d: %x\n", i, h->eth_src.addr[i]);
+    ENET_DEBUG("eth_dst:\n");
+    for (int i = 0; i < 6; i++)
+        ENET_DEBUG("%d: %x\n", i, h->eth_dst.addr[i]);
 }
 
-errval_t handle_ARP(struct enet_queue* q, struct devq_buf* buf,
-                    lvaddr_t vaddr) {
-    struct arp_hdr *header = (struct arp_hdr*) ((char *)vaddr + ETH_HLEN);
-    print_arp_packet(header);
+static errval_t arp_request_handle(struct enet_queue* q, struct devq_buf* buf,
+                                   struct arp_hdr *h, struct enet_driver_state* st) {
+    // TODO: i guess  save src info, then reply with dest info? idk i never read the book
     return SYS_ERR_OK;
 }
 
-errval_t handle_packet(struct enet_queue* q, struct devq_buf* buf) {
+errval_t handle_ARP(struct enet_queue* q, struct devq_buf* buf,
+                    lvaddr_t vaddr, struct enet_driver_state* st) {
+    struct arp_hdr *header = (struct arp_hdr*) ((char *)vaddr + ETH_HLEN);
+    print_arp_packet(header);
+
+    int opc = ntohs(header->opcode);
+    if (opc == 1) {
+        ENET_DEBUG("ARP Request\n");
+        arp_request_handle(q, buf, header, st);
+    } else if (opc == 2) {
+        ENET_DEBUG("ARP Reply\n");
+    } else {
+        ENET_DEBUG("Unknown ARP opcode\n");
+        // TODO: add errorcode to return here maybe?
+    }
+    return SYS_ERR_OK;
+}
+
+errval_t handle_packet(struct enet_queue* q, struct devq_buf* buf,
+                       struct enet_driver_state* st) {
     ENET_DEBUG("handling new packet\n");
     /* print_packet(q, buf); */
     struct region_entry *entry = get_region(q, buf->rid);
@@ -60,7 +84,7 @@ errval_t handle_packet(struct enet_queue* q, struct devq_buf* buf) {
     switch (htons(ETH_TYPE(header))) {
     case ETH_TYPE_ARP:
         ENET_DEBUG("Got an ARP packet!\n");
-        handle_ARP(q, buf, vaddr);
+        handle_ARP(q, buf, vaddr, st);
         break;
     case ETH_TYPE_IP:
         ENET_DEBUG("Got an IP packet!\n");
--- usr/drivers/enet/enet_handler.h
+++ usr/drivers/enet/enet_handler.h
@@ -1,3 +1,4 @@
 errval_t handle_ARP(struct enet_queue* q, struct devq_buf* buf,
-                    lvaddr_t p_addr);
-errval_t handle_packet(struct enet_queue* q, struct devq_buf* buf);
+                    lvaddr_t p_addr, struct enet_driver_state* st);
+errval_t handle_packet(struct enet_queue* q, struct devq_buf* buf,
+                       struct enet_driver_state* st);
