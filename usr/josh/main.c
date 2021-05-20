e3cea8f7dcf4eb9d2c49e4afc6589bb621912881
Nicolas Winkler <nicolas.winkler@gmx.ch>
2021-05-20 13:50:27 +0200

    moving process handling to different file

--- usr/josh/main.c
+++ usr/josh/main.c
@@ -8,15 +8,12 @@
 
 #include "ast.h"
 #include "builtins.h"
+#include "running.h"
 
+enum shell_state current_state;
 
 struct josh_line *parsed_line;
 
-struct aos_datachan process_chan;
-struct aos_datachan processin_chan;
-struct aos_rpc *process_rpc;
-
-void process_running(void);
 
 static void spawn_program(const char *name, struct array_list *args)
 {
@@ -72,31 +69,32 @@ static void spawn_program(const char *name, struct array_list *args)
         goto free_resources;
     }
 
-    aos_dc_init(&process_chan, 1024);
-    aos_dc_init(&processin_chan, 1024);
-    endpoint_create(LMP_RECV_LENGTH * 64, &process_chan.channel.lmp.local_cap, &process_chan.channel.lmp.endpoint);
+    struct running_program program;
+
+    aos_dc_init(&program.process_in, 1024);
+    aos_dc_init(&program.process_out, 1024);
+    endpoint_create(LMP_RECV_LENGTH * 64, &program.process_out.channel.lmp.local_cap, &program.process_out.channel.lmp.endpoint);
 
     void haendl(struct aos_rpc *r, struct capref ep, struct capref stdinep, struct capref *stdoutep) {
-        debug_printf("handle!\n");
         r->channel.lmp.remote_cap = ep;
-        processin_chan.channel.lmp.remote_cap = stdinep;
-        *stdoutep = process_chan.channel.lmp.local_cap;
-        //*stdoutep = stdout_chan.channel.lmp.remote_cap;
+        program.process_in.channel.lmp.remote_cap = stdinep;
+        *stdoutep = program.process_out.channel.lmp.local_cap;
     }
 
-    struct aos_rpc rpc;
-    process_rpc = &rpc;
-    aos_rpc_init_lmp(&rpc, lmp_ep_cap, NULL_CAP, lmp_ep, get_default_waitset());
-    aos_rpc_set_interface(&rpc, get_dispatcher_interface(), DISP_IFACE_N_FUNCTIONS, malloc(DISP_IFACE_N_FUNCTIONS * sizeof (void *)));
-    aos_rpc_register_handler(&rpc, DISP_IFACE_BINDING, haendl);
+    struct aos_rpc *rpc = &program.process_disprpc;
+    aos_rpc_init_lmp(rpc, lmp_ep_cap, NULL_CAP, lmp_ep, get_default_waitset());
+    aos_rpc_set_interface(rpc, get_dispatcher_interface(), DISP_IFACE_N_FUNCTIONS, malloc(DISP_IFACE_N_FUNCTIONS * sizeof (void *)));
+    aos_rpc_register_handler(rpc, DISP_IFACE_BINDING, haendl);
     //struct capref otherep;
 
-    while(capref_is_null(rpc.channel.lmp.remote_cap)) {
+    while(capref_is_null(rpc->channel.lmp.remote_cap)) {
         err = event_dispatch(get_default_waitset());
     }
 
-    process_running();
+    process_running(&program);
 
+    aos_dc_free(&program.process_in);
+    aos_dc_free(&program.process_out);
     printf("\n");
 
 free_resources:
@@ -107,57 +105,6 @@ free_resources:
 }
 
 
-void on_process_input(void *arg) {
-    struct lmp_endpoint *ep = arg;
-    errval_t err;
-
-    char buffer[1024];
-    size_t recvd;
-    do {
-        err = aos_dc_receive_available(&process_chan, sizeof buffer, buffer, &recvd);
-        if (recvd > 0)
-            err = aos_dc_send(&stdout_chan, recvd, buffer);
-    } while(recvd > 0);
-
-    if (aos_dc_is_closed(&process_chan)) {
-        return;
-    }
-
-    lmp_endpoint_register(ep, get_default_waitset(), MKCLOSURE(on_process_input, arg));
-}
-
-void on_console_input(void *arg) {
-    struct lmp_endpoint *ep = arg;
-    errval_t err;
-
-    char buffer[1024];
-    size_t recvd;
-    do {
-        err = aos_dc_receive_available(&stdin_chan, sizeof buffer, buffer, &recvd);
-        if (recvd > 0) {
-            if (buffer[0] == 3) {
-                aos_rpc_call(process_rpc, DISP_IFACE_TERMINATE);
-            }
-            else {
-                err = aos_dc_send(&processin_chan, recvd, buffer);
-            }
-        }
-    } while(recvd > 0);
-
-    lmp_endpoint_register(ep, get_default_waitset(), MKCLOSURE(on_console_input, arg));
-}
-
-
-void process_running(void)
-{
-    lmp_endpoint_register(process_chan.channel.lmp.endpoint, get_default_waitset(), MKCLOSURE(on_process_input, process_chan.channel.lmp.endpoint));
-    lmp_endpoint_register(stdin_chan.channel.lmp.endpoint, get_default_waitset(), MKCLOSURE(on_console_input, stdin_chan.channel.lmp.endpoint));
-    errval_t err;
-    while(!aos_dc_is_closed(&process_chan)) {
-        err = event_dispatch(get_default_waitset());
-    }
-}
-
 
 static void execute_command(struct josh_line *line)
 {
@@ -165,10 +112,7 @@ static void execute_command(struct josh_line *line)
         run_builtin(line);
     }
     else {
-
         spawn_program(line->cmd, &line->args);
-
-        //printf("unknown command: %s\n", line->cmd);
     }
 }
 
