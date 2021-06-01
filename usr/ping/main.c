315297487316536e918322411450671d69d633c5
nigglir <nigglir@student.ethz.ch>
2021-06-01 11:48:26 +0200

    adds commandline arguments to ping

--- usr/ping/main.c
+++ usr/ping/main.c
@@ -7,9 +7,31 @@
 #define MK_IP(a,b,c,d) (((a)<<24)|((b)<<16)|((c)<<8)|(d))
 int main(int argc, char **argv) {
     errval_t err;
-    /* uint32_t tgt_ip = MK_IP(169, 254, 6, 85); */
-    uint32_t tgt_ip = MK_IP(192, 168, 1, 34);
-    printf("pinging %d\n", tgt_ip);
+    if (argc < 3) {
+        printf("invalid number of arguments!\n");
+    }
+
+    // parse ip
+    char curn[20];
+    uint32_t tgt_ip = 0;
+    int curi = 0;
+    int lim = strlen(argv[1]);
+    for (int i = 0; i < 4; i++) {
+        int ti = 0;
+        while (curi < lim && argv[1][curi] != '.') {
+            curn[ti++] = argv[1][curi++];
+        }
+        curi++;
+
+        curn[ti] = '\0';
+        tgt_ip |= atoi(curn) << (3 - i) * 8;
+        printf("this time: %d\n", atoi(curn));
+    }
+
+    int count = atoi(argv[2]);
+
+    printf("pinging %s, %d times\n", argv[1], count);
+
     struct aos_ping_socket sock;
     err = aos_ping_init(&sock, tgt_ip);
     if (err_is_fail(err)) {
@@ -23,14 +45,12 @@ int main(int argc, char **argv) {
 
     uint16_t ackd = 0;
 
-    for (int i = 0; i < 10//00000
-             ; i++) {
+    while (ackd < count) {
         aos_ping_send(&sock);
         uint16_t a2 = aos_ping_recv(&sock);
         if (a2 > ackd) {
             printf("received packed %d!\n", a2);
             ackd = a2;
-            break;
         }
     }
 
