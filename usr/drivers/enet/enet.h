35ffaf5992702e3e35a9dd984fdf3fab408a6083
nigglir <nigglir@student.ethz.ch>
2021-06-01 12:24:12 +0200

    fixes arp requests & more usable ip address

--- usr/drivers/enet/enet.h
+++ usr/drivers/enet/enet.h
@@ -27,7 +27,8 @@ void inline ENET_DEBUG_UI32_AS_IP(char *msg, uint32_t ip) {
 void inline ENET_DEBUG_UI32_AS_IP(char *msg, uint32_t ip) {}
 #endif
 
-#define STATIC_ENET_IP 0x0a000201  // ip address of enet interface -> 10.0.2.1
+/* #define STATIC_ENET_IP 0x0a000201  // ip address of enet interface -> 10.0.2.1 */
+#define STATIC_ENET_IP 0xc0a8017d  // ip address of enet interface -> 192.168.1.125
 #define UDP_ECHO_PORT 2521  // TODO: use morse-code here
 
 #define ENET_PROMISC
