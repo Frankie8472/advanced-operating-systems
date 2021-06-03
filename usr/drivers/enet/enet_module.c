31fdf9d6c2a3546c87db38977948d19d666dd4a3
nigglir <nigglir@student.ethz.ch>
2021-06-03 14:23:05 +0200

    use correct macro

--- usr/drivers/enet/enet_module.c
+++ usr/drivers/enet/enet_module.c
@@ -723,7 +723,7 @@ int main(int argc, char *argv[]) {
     struct devq_buf buf;
     while(true) {
         err = event_dispatch_non_block(get_default_waitset());
-        if(err == LIB_ERR_NO_EVENT) {
+        if(err_no(err) == LIB_ERR_NO_EVENT) {
             err = devq_dequeue((struct devq*) st->rxq, &buf.rid, &buf.offset,
                                &buf.length, &buf.valid_data, &buf.valid_length,
                                &buf.flags);
